62a90fc22c587732cdece58828aa3b57b07d8d67
2021-DEV2-053 <90900639+2021-DEV2-053@users.noreply.github.com>
2021-09-21 13:00:58 +0200

    implement NewGameFragmentTest

--- TicTacToe/app/src/androidTest/java/mytest/tictactoe/ui/NewGameFragmentTest.kt
+++ TicTacToe/app/src/androidTest/java/mytest/tictactoe/ui/NewGameFragmentTest.kt
@@ -1,24 +1,29 @@
 package mytest.tictactoe.ui
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import mytest.tictactoe.R
 import mytest.tictactoe.launchFragmentInHiltContainer
 import mytest.tictactoe.ui.newgame.NewGameFragment
 import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+/**
+ * Integration test for the NewGame screen.
+ */
 @HiltAndroidTest
 class NewGameFragmentTest {
 
@@ -32,25 +37,25 @@ class NewGameFragmentTest {
 
     @Test
     fun testNavigationToInGameFragment() {
-        // Create a TestNavHostController
-        val navController = TestNavHostController(
-            ApplicationProvider.getApplicationContext())
+        // GIVEN - On the "Add Task" screen.
+        val navController = TestNavHostController(getApplicationContext())
+        launchFragment(navController)
 
-        // Create a graphical FragmentScenario for the NewGameFragment
-        val titleScenario = launchFragmentInHiltContainer<NewGameFragment>()
+        // WHEN - Valid playerX and playerO and click start
+        onView(withId(R.id.player_x_autoCompleteTextView)).perform(replaceText("john"))
+        onView(withId(R.id.player_o_autoCompleteTextView)).perform(replaceText("smith"))
+        onView(withId(R.id.start_button)).perform(click())
 
-       /* titleScenario.onFragment { fragment ->
-            // Set the graph on the TestNavHostController
+        // THEN - Verify that we navigated to the InGame screen.
+        assertEquals(navController.currentDestination?.id, R.id.inGameFragment)
+    }
+
+    private fun launchFragment(navController: TestNavHostController) {
+        launchFragmentInHiltContainer<NewGameFragment>() {
             navController.setGraph(R.navigation.nav_graph)
-
-            // Make the NavController available via the findNavController() APIs
-            Navigation.setViewNavController(fragment.requireView(), navController)
-        }*/
-
-        // Verify that performing a click changes the NavController’s state
-        onView(ViewMatchers.withId(R.id.start_button)).perform(ViewActions.click())
-
-        assertThat(navController.currentDestination?.id, equalTo(R.id.inGameFragment))
+            navController.setCurrentDestination(R.id.newGameFragment)
+            Navigation.setViewNavController(requireView(), navController)
+        }
     }
 
 }
