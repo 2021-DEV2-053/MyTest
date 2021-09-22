bcd4894d5677b037d45de7a71ea27d9192c6e3eb
2021-DEV2-053 <90900639+2021-DEV2-053@users.noreply.github.com>
2021-09-22 21:08:43 +0200

    Update PlayersAdapter.kt
    fix AutoCompleteTextview display

--- TicTacToe/app/src/main/java/mytest/tictactoe/ui/newgame/PlayersAdapter.kt
+++ TicTacToe/app/src/main/java/mytest/tictactoe/ui/newgame/PlayersAdapter.kt
@@ -5,27 +5,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Filter
 import android.widget.TextView
 import mytest.tictactoe.R
 import mytest.tictactoe.domain.model.Player
+import java.util.*
 
 class PlayersAdapter(
     val ctx: Context,
-    val players: List<Player>
-): ArrayAdapter<Player>(ctx, R.layout.item_player, players) {
+    val allPlayers: List<Player>
+): ArrayAdapter<Player>(ctx, R.layout.item_player, allPlayers) {
 
-    val layoutInflater = ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    private var players: List<Player> = allPlayers
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val player: Player? = getItem(position)
-        var view: View? = convertView
-        if(convertView == null){
-            view = layoutInflater.inflate(R.layout.item_player, null) as View
+        var convertView = convertView
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(
+                R.layout.item_player, parent, false
+            )
         }
-        view!!.findViewById<TextView>(R.id.name_textView).text = player!!.name
-
-        return view
+        val textViewName = convertView!!.findViewById<TextView>(R.id.name_textView)
+        val player: Player? = getItem(position)
+        if (player != null) {
+            textViewName.text = player.name
+        }
+        return convertView
     }
 
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun publishResults(charSequence: CharSequence?, filterResults: Filter.FilterResults) {
+                players = filterResults.values as List<Player>
+                notifyDataSetChanged()
+            }
+
+            override fun performFiltering(charSequence: CharSequence?): Filter.FilterResults {
+                val queryString = charSequence?.toString()?.lowercase(Locale.getDefault())
+
+                val filterResults = FilterResults()
+                filterResults.values = if (queryString==null || queryString.isEmpty())
+                    allPlayers
+                else
+                    allPlayers.filter {
+                        it.name!!.lowercase(Locale.getDefault()).contains(queryString)
+                    }
+                return filterResults
+            }
+        }
+    }
 
 }
